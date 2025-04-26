a7e961adf93fb3be4eb7153bf3c42fd2ea73a2b2
shahariaz <shahariaz.info@gmail.com>
2025-04-26 21:35:50 +0600

    feat: Hositing in JS

---
+++ Javascript/hoisting/index.js
@@ -0,0 +1,12 @@
+getName();
+console.log(x);
+console.log(y);
+console.log(getName);
+x = 20;
+var x = 5;
+var y = 10;
+console.log(x);
+console.log(y);
+function getName() {
+  console.log("getName function called");
+}
